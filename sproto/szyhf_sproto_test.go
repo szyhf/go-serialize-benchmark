35b0619cdc8f8b8647e8a3be75636961471b0e8f
Back Yu <yhfszb@gmail.com>
2017-03-01 15:25:14 +0800

    更新sproto的压测，使用了gosprot的原版和自己的变态版

--- sproto/szyhf_sproto_test.go
+++ sproto/szyhf_sproto_test.go
@@ -4,57 +4,74 @@ import (
 	"math"
 	"testing"
 
-	convert "go.szyhf.org/di-convert"
-	"go.szyhf.org/digo/log"
-
 	sproto "github.com/szyhf/gosproto"
 )
 
-func TestEncodeSProto(t *testing.T) {
-	msg := newValMsg()
-	d, e := sproto.EncodePacked(msg)
-	log.Debug("EncodePackedProto3.Len=", len(d), e)
-
-	dMsg := &ValMSG{}
-	sproto.DecodePacked(d, dMsg)
-	log.Debug(convert.MustJsonString(dMsg))
-}
-
-func BenchmarkSProtoEncodeVal(b *testing.B) {
+func BenchmarkSZYHFEncodeSproto(b *testing.B) {
 	b.StopTimer()
 	msg := newValMsg()
+	_, err := sproto.Encode(msg)
+	if err != nil {
+		b.Errorf("sproto.Encode failed:", err)
+		return
+	}
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		sproto.Encode(msg)
 	}
 }
 
-func BenchmarkSProtoEncodePackedVal(b *testing.B) {
+func BenchmarkSZYHFDecodeSproto(b *testing.B) {
 	b.StopTimer()
 	msg := newValMsg()
+	d, err := sproto.Encode(msg)
+	if err != nil {
+		b.Errorf("sproto.Encode failed:", err)
+		return
+	}
+	nMsg := new(ValMSG)
+	_, err = sproto.Decode(d, nMsg)
+	if err != nil {
+		b.Errorf("sproto.Decode failed:", err)
+		return
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sproto.Decode(d, nMsg)
+	}
+}
+
+func BenchmarkSZYHFEncodePackedSproto(b *testing.B) {
+	b.StopTimer()
+	msg := newValMsg()
+	_, err := sproto.EncodePacked(msg)
+	if err != nil {
+		b.Errorf("sproto.EncodePacked failed:", err)
+		return
+	}
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		sproto.EncodePacked(msg)
 	}
 }
 
-func BenchmarkSProtoDecodeVal(b *testing.B) {
+func BenchmarkSZYHFDecodePackedSproto(b *testing.B) {
 	b.StopTimer()
-	msgData, _ := sproto.Encode(newValMsg())
-	var msg = ValMSG{}
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		sproto.Decode(msgData, &msg)
+	msg := newValMsg()
+	d, err := sproto.EncodePacked(msg)
+	if err != nil {
+		b.Errorf("sproto.EncodePacked failed:", err)
+		return
 	}
-}
-
-func BenchmarkSProtoDecodePackedVal(b *testing.B) {
-	b.StopTimer()
-	msgData, _ := sproto.EncodePacked(newValMsg())
-	var msg = ValMSG{}
+	nMsg := new(ValMSG)
+	err = sproto.DecodePacked(d, nMsg)
+	if err != nil {
+		b.Errorf("sproto.DecodePacked failed:", err)
+		return
+	}
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		sproto.DecodePacked(msgData, &msg)
+		sproto.DecodePacked(d, nMsg)
 	}
 }
 
---
+++ sproto/xjdrew_sproto_test.go
@@ -0,0 +1,135 @@
+package sproto
+
+import (
+	"math"
+	"testing"
+
+	sproto "github.com/szyhf/gosproto"
+)
+
+func TestEncodeSproto(t *testing.T) {
+	msg := newMsg()
+	d, err := sproto.Encode(msg)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log("SprotoEncode.Len=", len(d))
+
+	d, err = sproto.EncodePacked(msg)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log("SprotoEncodePacked.Len=", len(d))
+}
+
+func BenchmarkEncodeSproto(b *testing.B) {
+	b.StopTimer()
+	msg := newMsg()
+	_, err := sproto.Encode(msg)
+	if err != nil {
+		b.Errorf("sproto.Encode failed:", err)
+		return
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sproto.Encode(msg)
+	}
+}
+
+func BenchmarkDecodeSproto(b *testing.B) {
+	b.StopTimer()
+	msg := newMsg()
+	d, err := sproto.Encode(msg)
+	if err != nil {
+		b.Errorf("sproto.Encode failed:", err)
+		return
+	}
+	nMsg := new(PtrMSG)
+	_, err = sproto.Decode(d, nMsg)
+	if err != nil {
+		b.Errorf("sproto.Decode failed:", err)
+		return
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sproto.Decode(d, nMsg)
+	}
+}
+
+func BenchmarkEncodePackedSproto(b *testing.B) {
+	b.StopTimer()
+	msg := newMsg()
+	_, err := sproto.EncodePacked(msg)
+	if err != nil {
+		b.Errorf("sproto.EncodePacked failed:", err)
+		return
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sproto.EncodePacked(msg)
+	}
+}
+
+func BenchmarkDecodePackedSproto(b *testing.B) {
+	b.StopTimer()
+	msg := newMsg()
+	d, err := sproto.EncodePacked(msg)
+	if err != nil {
+		b.Errorf("sproto.EncodePacked failed:", err)
+		return
+	}
+	nMsg := new(PtrMSG)
+	err = sproto.DecodePacked(d, nMsg)
+	if err != nil {
+		b.Errorf("sproto.DecodePacked failed:", err)
+		return
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sproto.DecodePacked(d, nMsg)
+	}
+}
+
+func newMsg() *PtrMSG {
+	return &PtrMSG{
+		Int:         sproto.Int(math.MaxInt64),
+		IntNeg:      sproto.Int(math.MinInt32 + 1),
+		String:      sproto.String("Hello"),
+		Bool:        sproto.Bool(true),
+		ByteSlice:   []byte("World"),
+		BoolSlice:   []bool{true, true, false, true, false},
+		IntSlice:    []int{123, 321, 1234567},
+		StringSlice: []string{"FOO", "BAR"},
+		Struct: &HoldPtrMSG{
+			Int:         sproto.Int(1),
+			String:      sproto.String("Hello"),
+			Bool:        sproto.Bool(true),
+			ByteSlice:   []byte("World"),
+			BoolSlice:   []bool{true, true, false, true, false},
+			IntSlice:    []int{123, 321, 1234567},
+			StringSlice: []string{"FOO", "BAR"},
+		},
+		StructSlice: []*HoldPtrMSG{
+			&HoldPtrMSG{
+				Int:         sproto.Int(2),
+				String:      sproto.String("Hello2"),
+				Bool:        sproto.Bool(true),
+				ByteSlice:   []byte("World2"),
+				BoolSlice:   []bool{true, true, false, true, false},
+				IntSlice:    []int{123, 321, 1234567},
+				StringSlice: []string{"FOO2", "BAR2"},
+			},
+			&HoldPtrMSG{
+				Int:         sproto.Int(3),
+				String:      sproto.String("Hello3"),
+				Bool:        sproto.Bool(true),
+				ByteSlice:   []byte("World3"),
+				BoolSlice:   []bool{true, true, false, true, false},
+				IntSlice:    []int{123, 321, 1234567},
+				StringSlice: []string{"FOO3", "BAR3"},
+			},
+		},
+	}
+}
