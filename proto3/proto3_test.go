8260d7d60ede99e67fbdb54ee922836172c72fe1
Back Yu <yhfszb@gmail.com>
2017-03-01 12:35:20 +0800

    增加proto3的压测

---
+++ proto3/proto3_test.go
@@ -0,0 +1,85 @@
+package proto3
+
+import (
+	"math"
+	"testing"
+
+	convert "go.szyhf.org/di-convert"
+	"go.szyhf.org/digo/log"
+
+	"encoding/json"
+
+	"github.com/golang/protobuf/proto"
+)
+
+func TestEncodeProto3(t *testing.T) {
+	msg := newValMsg()
+	d, e := proto.Marshal(msg)
+	log.Debug("EncodeProto3.Len=", len(d), e)
+
+	dMsg := &ValMsg{}
+	proto.Unmarshal(d, dMsg)
+	log.Debug(convert.MustJsonString(dMsg))
+}
+
+func BenchmarkEncodeProto3(b *testing.B) {
+	b.StopTimer()
+	msg := newValMsg()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		proto.Marshal(msg)
+	}
+}
+
+func BenchmarkDecodeProto3(b *testing.B) {
+	b.StopTimer()
+	msg := newValMsg()
+	d, _ := proto.Marshal(msg)
+	nMsg := &ValMsg{}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		json.Unmarshal(d, nMsg)
+	}
+}
+
+func newValMsg() *ValMsg {
+	return &ValMsg{
+		Int:         math.MaxInt64,
+		IntNeg:      math.MinInt32 + 1,
+		Str:         "Hello",
+		Bool:        true,
+		ByteSlice:   []byte("World"),
+		BoolSlice:   []bool{true, true, false, true, false},
+		IntSlice:    []int64{123, 321, 1234567},
+		StringSlice: []string{"FOO", "BAR"},
+		Struct: &HoldValMsg{
+			Int:         1,
+			Str:         "Hello",
+			Bool:        true,
+			ByteSlice:   []byte("World"),
+			BoolSlice:   []bool{true, true, false, true, false},
+			IntSlice:    []int64{123, 321, 1234567},
+			StringSlice: []string{"FOO", "BAR"},
+		},
+		StructSlice: []*HoldValMsg{
+			&HoldValMsg{
+				Int:         2,
+				Str:         "Hello2",
+				Bool:        true,
+				ByteSlice:   []byte("World2"),
+				BoolSlice:   []bool{true, true, false, true, false},
+				IntSlice:    []int64{123, 321, 1234567},
+				StringSlice: []string{"FOO2", "BAR2"},
+			},
+			&HoldValMsg{
+				Int:         3,
+				Str:         "Hello3",
+				Bool:        true,
+				ByteSlice:   []byte("World3"),
+				BoolSlice:   []bool{true, true, false, true, false},
+				IntSlice:    []int64{123, 321, 1234567},
+				StringSlice: []string{"FOO3", "BAR3"},
+			},
+		},
+	}
+}
