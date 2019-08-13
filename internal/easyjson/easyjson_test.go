7550381a214f6d73ed2e983468b6e3a39eb8bf3a
Back Yu <yhfszb@gmail.com>
2019-08-13 12:06:14 +0800

    增加easyjson并更新测试结果

---
+++ internal/easyjson/easyjson_test.go
@@ -0,0 +1,87 @@
+package easyjson
+
+import (
+	"math"
+	"testing"
+)
+
+func TestEncodeEasyJSON(t *testing.T) {
+	msg := newValMsg()
+	var d []byte
+	d, err := msg.MarshalJSON()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log("EncodeMsgp.Len=", len(d))
+
+	dMsg := &ValMsg{}
+	err = dMsg.UnmarshalJSON(d)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func BenchmarkEncodeEasyJSON(b *testing.B) {
+	b.StopTimer()
+	msg := newValMsg()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		msg.MarshalJSON()
+	}
+}
+
+func BenchmarkDecodeEasyJSON(b *testing.B) {
+	b.StopTimer()
+	msg := newValMsg()
+	var d []byte
+	d, _ = msg.MarshalJSON()
+	nMsg := &ValMsg{}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		nMsg.UnmarshalJSON(d)
+	}
+}
+
+func newValMsg() *ValMsg {
+	return &ValMsg{
+		Int:         math.MaxInt64,
+		IntNeg:      math.MinInt32 + 1,
+		Str:         "Hello",
+		Bool:        true,
+		ByteSlice:   []byte("World"),
+		BoolSlice:   []bool{true, true, false, true, false},
+		IntSlice:    []int{123, 321, 1234567},
+		StringSlice: []string{"FOO", "BAR"},
+		Struct: &HoldValMsg{
+			Int:         1,
+			Str:         "Hello",
+			Bool:        true,
+			ByteSlice:   []byte("World"),
+			BoolSlice:   []bool{true, true, false, true, false},
+			IntSlice:    []int{123, 321, 1234567},
+			StringSlice: []string{"FOO", "BAR"},
+		},
+		StructSlice: []*HoldValMsg{
+			&HoldValMsg{
+				Int:         2,
+				Str:         "Hello2",
+				Bool:        true,
+				ByteSlice:   []byte("World2"),
+				BoolSlice:   []bool{true, true, false, true, false},
+				IntSlice:    []int{123, 321, 1234567},
+				StringSlice: []string{"FOO2", "BAR2"},
+			},
+			&HoldValMsg{
+				Int:         3,
+				Str:         "Hello3",
+				Bool:        true,
+				ByteSlice:   []byte("World3"),
+				BoolSlice:   []bool{true, true, false, true, false},
+				IntSlice:    []int{123, 321, 1234567},
+				StringSlice: []string{"FOO3", "BAR3"},
+			},
+		},
+	}
+}
